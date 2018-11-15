5038984d0c041bd76982d4e161fb07bb3e5533e3
noax <thomasbacks@gmail.com>
2018-11-15 17:15:35 -0500

    Added Main Menu in the game

---
+++ Assets/Script/LoadOnClick.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadOnClick : MonoBehaviour
+{
+    public void LoadScene(int level) {
+        SceneManager.LoadScene(level);
+    }
+
+    public void ExitGame() {
+        Application.Quit();
+    }
+}
---
+++ Assets/Script/ReturnToMenu.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReturnToMenu : MonoBehaviour
+{
+    private void Update() {
+        if(Input.GetButtonDown("Start")) {
+            Application.LoadLevel(0);
+        }
+    }
+}
