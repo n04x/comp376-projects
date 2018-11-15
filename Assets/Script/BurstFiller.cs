48c69d8a93ea0a1bbe0cade6b3d38cd06b234b90
AlSo1992 <43284615+AlSo1992@users.noreply.github.com>
2018-11-15 07:16:03 -0500

    Burst UI
    - Optimized burst meter assets
         - New meters with optimized fill
         - Placeholder white suit sprites for 21, will add VFX later to improve aesthetics
         - Half heart sprites are now available
    - Burst meter now displays total score, individual suit scores, and timer
         - Timer currently does nothing; BlackjackController has a comment in all caps in Update() to know where to implement the timeout checks.
    - Added a drawn cards demo to the UI; can be enabled to see what drawn cards might look like in final version.

--- Assets/Script/BurstFiller.cs
+++ Assets/Script/BurstFiller.cs
@@ -1,10 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BurstFiller : MonoBehaviour
 {
-    SpriteRenderer spriteRenderer;
+    Image fillImage;
+    public GameObject burstImage;
     int meter;
     public Sprite[] fillers;
     public int index;
@@ -12,7 +14,8 @@ public class BurstFiller : MonoBehaviour
     private void Awake() {
         blackjack = false;
         meter = 0;
-        spriteRenderer = GetComponent<SpriteRenderer>();        
+        fillImage = GetComponent<Image>();   
+        fillImage.enabled = false;    
     }
 
     public void fillMeter(int[] card) {
@@ -20,32 +23,44 @@ public class BurstFiller : MonoBehaviour
         float percentage = ( (float) meter / 21) * 100.0f;
         Debug.Log("fillMeter(): " + percentage);
         if(blackjack) {
-            spriteRenderer.sprite = fillers[9];
+            fillImage.enabled = true;
+            fillImage.sprite = fillers[9];
         }else if(meter == 0) { 
-            spriteRenderer.sprite = null;
+            fillImage.enabled = false;
         } else if(percentage > 0.0f && percentage < 10.0f) {
-            spriteRenderer.sprite = fillers[0];
+            fillImage.enabled = true;
+            fillImage.sprite = fillers[0];
         } else if(percentage >= 10.0f && percentage < 20.0f) {
-            spriteRenderer.sprite = fillers[1];
+            fillImage.enabled = true;
+            fillImage.sprite = fillers[1];
         } else if(percentage >= 20.0f && percentage < 30.0f) {
-            spriteRenderer.sprite = fillers[2];
+            fillImage.enabled = true;
+            fillImage.sprite = fillers[2];
         } else if(percentage >= 30.0f && percentage < 40.0f) {
-            spriteRenderer.sprite = fillers[3];
+            fillImage.enabled = true;
+            fillImage.sprite = fillers[3];
         } else if(percentage >= 40.0f && percentage < 50.0f) {
-            spriteRenderer.sprite = fillers[4];
+            fillImage.enabled = true;
+            fillImage.sprite = fillers[4];
         } else if(percentage >= 50.0f && percentage < 60.0f) {
-            spriteRenderer.sprite = fillers[5];
+            fillImage.enabled = true;
+            fillImage.sprite = fillers[5];
         } else if(percentage >= 60.0f && percentage < 70.0f) {
-            spriteRenderer.sprite = fillers[6];
+            fillImage.enabled = true;
+            fillImage.sprite = fillers[6];
         } else if(percentage >= 70.0f && percentage < 80.0f) {
-            spriteRenderer.sprite = fillers[7];            
+            fillImage.enabled = true;
+            fillImage.sprite = fillers[7];            
         } else if(percentage >= 80.0f && percentage < 90.0f) {
-            spriteRenderer.sprite = fillers[8];
+            fillImage.enabled = true;
+            fillImage.sprite = fillers[8];
         } else if(percentage >= 90.0f) {
-            spriteRenderer.sprite = fillers[9];
-        }
+            fillImage.enabled = true;
+            fillImage.sprite = fillers[9];
+        } 
     }
     public void SetBlackJack(bool b) {
         blackjack = b;
+        burstImage.SetActive(b);
     }
 }
