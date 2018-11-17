fc731c8b99ef62285bd5ea8745499fb1e49dfea0
Pandaromin <earlsteven.aromin@mail.concordia.ca>
2018-11-17 16:48:53 -0500

    removes unused sword

--- Packages/Assets/Script/Alice/AliceSword.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class AliceSword : MonoBehaviour
-{
-    // Start is called before the first frame update
-  public int damage = 1;
-}
