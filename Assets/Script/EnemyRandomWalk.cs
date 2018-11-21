6236d1540948e4cb09c11a2c8def32f9e7e20ff7
noax <thomasbacks@gmail.com>
2018-11-20 21:06:53 -0500

    Remove the Debug.Log about enemies and add Game Over display with a delay of 5 seconds before returning to main menu #13

--- Assets/Script/EnemyRandomWalk.cs
+++ Assets/Script/EnemyRandomWalk.cs
@@ -67,13 +67,11 @@ public class EnemyRandomWalk : MonoBehaviour {
     {
         if(other.gameObject.tag == "Enemy" && gameObject.tag == "Enemy")
         {
-            Debug.Log("Bump");
             areaTarget.position = new Vector2(Random.Range(minCoordX, maxCoordX), Random.Range(minCoordY, maxCoordY));
         }
 
         if (other.gameObject.tag == "Player" && gameObject.tag == "Enemy")
         {
-            Debug.Log("Bump");
             enemy.RunFromTarget();
             areaTarget.position = new Vector2(Random.Range(minCoordX, maxCoordX), Random.Range(minCoordY, maxCoordY));
         }
@@ -83,7 +81,6 @@ public class EnemyRandomWalk : MonoBehaviour {
     {
         if (other.gameObject.tag == "Player" && gameObject.tag == "Enemy")
         {
-            Debug.Log("Bump");
             enemy.RunFromTarget();
             areaTarget.position = new Vector2(Random.Range(minCoordX, maxCoordX), Random.Range(minCoordY, maxCoordY));
         }
